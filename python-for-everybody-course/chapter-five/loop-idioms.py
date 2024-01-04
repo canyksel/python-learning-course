56ff1b3cbd49029864ab0a0e36c5144b7a109fcf
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2024-01-04 21:55:40 +0300

    chapter-5 finished

--- python-for-everybody-course/chapter-five/loop-idioms.py
+++ python-for-everybody-course/chapter-five/loop-idioms.py
@@ -7,3 +7,26 @@ for the_num in [9, 41, 12, 3, 74, 15] :
     print(largest_so_far, the_num)
 
 print('After', largest_so_far)
+
+#Extra: What is the largest and lowest number?
+largest_num= float('-inf')
+lowest_num = float('inf')
+
+for the_num in [9, 41, 12, 3, 74, 15] :
+    if the_num > largest_num:
+        largest_num = the_num
+    elif the_num < lowest_num:
+        lowest_num = the_num
+
+print('Largest', largest_num)
+print('Lowest', lowest_num)
+
+
+#Extra: Alternative of the finding largest and lowest number:
+numbers = [9, 41, 12, 3, 74, 15]
+lowest = min(numbers)
+largest = max(numbers)
+
+print('Largest', largest)
+print('Lowest', lowest)
+
--- python-for-everybody-course/chapter-five/more-loop-patterns.py
+++ python-for-everybody-course/chapter-five/more-loop-patterns.py
@@ -5,3 +5,81 @@ for thing in [9, 41, 12, 3, 74, 15] :
     zork = zork + 1
     print(zork, thing)
 print('After', zork)
+
+'''
+* To count how many times we execute a loop, we introduce a counter variable thant starts at 0 and we add one to it each time through the loop.
+'''
+
+#Summing in a Loop
+zork = 0 
+print('Before', zork)
+for thing in [9, 41, 12, 3, 74, 15] :
+    zork = zork + thing
+    print(zork, thing)
+print('After', zork)
+
+'''
+* To add up a value we encounter in a loop, we introduce a sum variable that starts at 0 and we add the value to the sum each time through the loop.
+'''
+
+#Finding the Average in a Loop
+count = 0
+sum = 0
+print('Before', count, sum)
+for value in [9, 41, 12, 3, 74, 15] :
+    count = count + 1
+    sum = sum + value
+    print(count,sum,value)
+print('After', count, sum, sum / count)
+
+'''
+* An average just combines the counting and sum patterns and divides whe the loop is done.
+'''
+
+#Filtering in a Loop
+print('Before')
+for value in [9, 41, 12, 3, 74, 15] :
+    if value > 20 :
+        print('Large number', value)
+print('After')
+
+'''
+* We use an if statement in the loop to catch / filter the values we are looking for.
+'''
+
+#Search Using a Boolean Variable
+found = False
+print('Before', found)
+for value in [9, 41, 12, 3, 74, 15] :
+    if value == 3 : 
+        found = True
+    print(found, value)
+print('After', found)
+
+'''
+* If we just want to search and know if a value was found, we use a variable that starts at False and is set to True as soon as we find what we are looking for.
+'''
+
+#How to find the smallest value
+smallest = None
+print('Before')
+for value in [9, 41, 12, 3, 74, 15] :
+    if smallest is None :
+        smallest = value
+    elif value < smallest :
+        smallest = value
+    print(smallest, value)
+print('After', smallest)
+
+'''
+* We still have a variable that is the smallest so far. The first time through the loop smallest is None, so we take the first value to be the smallest.
+'''
+
+# The "is" and "is not" Operators
+'''
+* Python has an is operator that can be used in logical expressions
+* Implies "is the same as"
+* Similar to, but stronger than ==
+* is not also is a logical operator
+For example 0 == 0.0 is return as "true" because it doesn't look a type. But if write 0 is 0.0 it returns "false" becase each one has a different type
+'''
