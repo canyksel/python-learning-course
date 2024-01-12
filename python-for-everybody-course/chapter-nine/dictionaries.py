9f4af296901d3620d6fa8a86773dbdca5bcdda7e
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2024-01-12 23:48:06 +0300

    chapter-9 finished

--- python-for-everybody-course/chapter-nine/dictionaries.py
+++ python-for-everybody-course/chapter-nine/dictionaries.py
@@ -78,3 +78,97 @@ ooo = {} # it is same meanings as dict()
 print(ooo)
 
 #Most Common Name
+#Many Counters with a Dictionary
+'''
+* One common use of dictionaries is counting how often we "see" something.
+'''
+ccc = dict()
+ccc['csev'] = 1
+ccc['cwen'] = 1
+print(ccc)
+ccc['cwen'] = ccc['cwen'] + 1
+print(ccc)
+
+#Dictionary Tracebacks
+'''
+* It is an error to reference a key which is not in the dictionary
+* We can use the in operator to see if a key is in the dictionary
+
+#Example
+ccc = dict()
+print(ccc[''])
+'csev' in ccc //False
+'''
+
+#When we see a new name
+'''
+* When we encounter a new name, we need to add a new entry in the dictionary and if this the second or later time we have seen the name, we simply add one to the count in the dictionary under that name
+'''
+
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names :
+    if name not in counts :
+        counts[name] = 1
+    else :
+        counts[name] = counts[name] + 1
+print(counts)
+
+#The get method for dictionaries
+'''
+* The pattern of checking to see if a key is already in a dictionary and assuming a default value if the key is not there is so common that there is a method called get() that does this for us
+* We can use get() and provide a default value of zero when the key is not yet in the dictionary - and then just add one
+'''
+
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names :
+    counts[name] = counts.get(name, 0) + 1
+print(counts)
+
+#Counting Pattern
+'''
+* The general pattern to count the words in a line of text is to split the line into words, then loop through the words and use a dictionary to track the count of each word independently.
+'''
+
+counts = dict()
+print('Enter a line of text:')
+line = input('')
+
+words = line.split()
+print('Words:', words)
+
+print('Counting...')
+for word in words :
+    counts[word] = counts.get(word,0) + 1
+print('Counts',counts)
+
+#Definite Loops and Dictionaries
+'''
+* Even though dictionaries are not stored in order, we can write a for loop that goes through all the entries in a dictionary - actually it goes through all of the keys in the dictionary and looks up the values
+'''
+
+counts = {'chuck' : 1, 'fred' : 42, 'jan': 100}
+for key in counts:
+    print(key, counts[key])
+
+#Retrieving lists of Keys and Values
+'''
+* You can get a list of keys, values, or items(both) from a dictionary
+'''
+
+jjj = {'chuck' : 1, 'fred' : 42, 'jan': 100}
+print(list(jjj))
+print(jjj.keys())
+print(jjj.values())
+print(jjj.items())
+
+#Bonus: Two Iteration Variables!
+'''
+* We loop through the key-value pairs in a dictionary using "two" iteration variables
+* Each iteration, the first variable is the key and the second variable is the corresponding value for the key
+'''
+
+jjj = {'chuck' : 1, 'fred' : 42, 'jan': 100}
+for aaa,bbb in jjj.items() :
+    print(aaa, bbb)
